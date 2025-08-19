ec3b149d6a56356feafa60c651a34d2d3fd0d67d
vladimirzhukov <zhukov.vladimir@gmail.com>
2025-08-19 21:27:15 +0300

    Update dependencies and fix LinkedIn URL format in profile: upgrade aws/aws-sdk-php to version 3.356.0; correct LinkedIn input label

--- resources/views/app/profile.blade.php
+++ resources/views/app/profile.blade.php
@@ -371,7 +371,7 @@
                     <label for="li" class="block text-sm/6 font-medium text-gray-900">LinkedIn</label>
                     <div class="mt-2">
                         <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                            <div class="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">linkedin.com/</div>
+                            <div class="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">linkedin.com/in/</div>
                             <input type="text" name="li" id="li" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="username" value="{{ (!empty(Auth::user()->profile->li) ? Auth::user()->profile->li : '') }}" />
                         </div>
                     </div>
